4cb3e888038855fc533183f77d31f9dc38da403e
wzrui <920707594@qq.com>
2017-08-04 13:14:32 +0800

    8.4

---
+++ Monitore/Monitore/Class/Patrol/Model/PatrolHistoryModel.h
@@ -0,0 +1,13 @@
+//
+//  PatrolHistoryModel.h
+//  Monitore
+//
+//  Created by kede on 2017/8/4.
+//  Copyright © 2017年 kede. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PatrolHistoryModel : NSObject
+
+@end
---
+++ Monitore/Monitore/Class/Patrol/Model/PatrolHistoryModel.m
@@ -0,0 +1,13 @@
+//
+//  PatrolHistoryModel.m
+//  Monitore
+//
+//  Created by kede on 2017/8/4.
+//  Copyright © 2017年 kede. All rights reserved.
+//
+
+#import "PatrolHistoryModel.h"
+
+@implementation PatrolHistoryModel
+
+@end
